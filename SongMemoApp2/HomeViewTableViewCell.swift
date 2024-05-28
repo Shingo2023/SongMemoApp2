15ce65584cdedc11de65252fd9e556933a9337d3
Shingo <taka31.main@gmail.com>
2024-05-28 23:23:30 +0900

    Add Xcode project files

---
+++ SongMemoApp2/HomeViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewTableViewCell.swift
+//  SongMemoApp2
+//
+//  Created by 高橋真悟 on 2024/05/03.
+//
+
+import UIKit
+
+class HomeViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SongMemoApp2/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SongMemoApp2
+//
+//  Created by 高橋真悟 on 2024/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var homeActionView: UILabel!
+    
+    
+    @IBOutlet weak var homeTableView: UITableView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
