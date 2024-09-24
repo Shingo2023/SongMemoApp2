fa067ce9232f318716a562d09ed23cabdfd9fd92
Shingo <taka31.main@gmail.com>
2024-09-24 20:02:45 +0900

    アクションビューにRealmのwriteメソッドを追記

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 概要説明
-- このRepositoryはGithub学習のために作成されました。11
+- このRepositoryはGithub学習のために作成されました。12
--- SongMemoApp2/SongTextModel.swift
+++ SongMemoApp2/SongTextModel.swift
@@ -12,12 +12,9 @@ import Foundation
 import RealmSwift
 //レルムを使うクラスにはObjectというクラスを継承。そのプロパティにはイニシャライザーが必要
 class SongTextModel: Object {
-    //idはこのモデルのプライマリキーで、各SongTextModelオブジェクトを一意に識別するための整数型のIDです。
-    //@Persisted:パシスティッド 属性により、Realmに保存されるプロパティであることが示されています。
-    //primaryKey: trueの指定により、このidプロパティがプライマリキーとして扱われ、同じidを持つオブジェクトは1つだけ存在できるようになります。
-    @Persisted(primaryKey: true) var id: Int = 0
-    //textは歌詞のテキストを保持するための文字列型プロパティです。
-    //デフォルト値は空文字列""に設定されています。
+    
+    @Persisted var id: String = UUID().uuidString
+    
     @Persisted var text: String = ""
     //colorRangeは、歌詞の一部に適用される色範囲を保持するためのリストです。
     //ColorRangeModelオブジェクトのリストとして定義されており、複数の色範囲を管理できます。
