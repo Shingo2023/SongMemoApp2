c9d1fed7c33fadafd305498cfe070e9e370fd46f
Shingo <taka31.main@gmail.com>
2024-09-30 15:15:01 +0900

    リリックビューの保存に難航中

--- SongMemoApp2/ActionEditViewController.swift
+++ SongMemoApp2/ActionEditViewController.swift
@@ -9,41 +9,33 @@ import Foundation
 import UIKit
 import RealmSwift
 
-class ActionEditViewController: UIViewController,UINavigationControllerDelegate {
+class ActionEditViewController: UIViewController,UINavigationControllerDelegate,UITextFieldDelegate {
     
     
     @IBOutlet weak var actionNameField1: UITextField!
-    
     @IBOutlet weak var actionMarkField1: UITextField!
-    
     @IBOutlet weak var actionNameField2: UITextField!
-    //@IBOutlet weak var actionNameField2: UITextField!
-    
     @IBOutlet weak var actionMarkField2: UITextField!
-    //@IBOutlet weak var actionMarkField2: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // UINavigationController のデリゲートを設定
         self.navigationController?.delegate = self
-        
         // カスタムツールバーをキーボードに追加
         addCustomToolbarToKeyboard()
+        
+        actionMarkField1.delegate = self
+        actionMarkField2.delegate = self
     }
     //アドカスタムツールバートゥキーボード
     func addCustomToolbarToKeyboard() {
         //ツールバーの位置
         let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 44))
-        
         // "Done"ボタンを作成
         let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneButtonTapped))
-        
-        // ボタンの間にスペースを作るためのフレキシブルスペースを作成
+        // ボタンの間にスペースを作るためのフレキシブルスペースを作成　spacer:スペーサー
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        
-        // ツールバーにボタンを追加
-        toolbar.items = [spacer, doneButton]
         
         // 各テキストフィールドにツールバーを設定
         actionNameField1.inputAccessoryView = toolbar
@@ -60,61 +52,53 @@ class ActionEditViewController: UIViewController,UINavigationControllerDelegate 
     
     // UINavigationControllerDelegate メソッド
     //    //新しいビューコントローラがナビゲーションスタックに表示される直前に呼び出されます。
-    //    //navigationController: このデリゲートメソッドを呼び出している
+    //    //navigationController: このデリゲートメソッドを呼び出している。
     //    //UINavigationController のインスタンス。
     //    //viewController: ナビゲーションコントローラによって表示されようとしているビューコントローラ。
     //    // animated: アニメーションが使用されるかどうかを示すブール値。
-    // UINavigationControllerDelegate メソッド
+    // UINavigationControllerDelegate
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-        //viewControllerではない時にtureを出す条件式 つまり他のページに遷移したときに保存処理が実行される
-        //selfはActionEditViewControllerのインスタンス化
-        // 他のページに遷移したときに保存処理を実行
         if viewController != self {
-            saveActions(with: action1)
-            saveActions(with: action2)
+            // guard let がnilであればelseが実行される
+            //1文字ならnilではない
+            guard let actionMark1 = actionMarkField1.text, actionMark1.count == 1,
+                  let actionMark2 = actionMarkField2.text, actionMark2.count == 1 else {
+                // nilの時に実行されるエラーメッセージ
+                let alert = UIAlertController(title: "エラー", message: "アクションマークは1文字の絵文字である必要があります。", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                present(alert, animated: true)
+                navigationController.popViewController(animated: false) // 戻るのをキャンセル
+                return
+            }
+            saveActions()
         }
     }
     
     //　アクションを保存するメソッド
-    func saveActions(with updatedText: String) {
-        // 各テキストフィールドからデータを取得し、ActionModelインスタンスを作成
+    func saveActions() {
+        guard let actionMark1 = actionMarkField1.text, actionMark1.count == 1,
+              let actionMark2 = actionMarkField2.text, actionMark2.count == 1 else {
+            print("アクションマークが無効です。")
+            return
+        }
+        
         let action1 = ActionModel()
         let action2 = ActionModel()
+        action1.mark = actionMark1
+        action2.mark = actionMark2
         
-        //テキストフィールドからデータを取得
-        //if let オプショナル型の値を安全にアンラップするための構文
-        //        if let 定数名 = オプショナル値 {
-        //            // オプショナル値がnilでない場合に実行される処理
-        //        } else {
-        //            // オプショナル値がnilの場合に実行される処理（任意）
-        //        }
-        //ローカル定数actionName1 = UItextFieldとテキストプロパティ　Fieldの文字数が１より少ない
-        if let actionMark1 = actionMarkField1.text, actionMark1.count <= 1 {
-            action1.mark = actionMark1
-        } else {// 1文字より多い場合のエラー
-            print("アクションマークは1文字の絵文字である必要があります。")
-        }
-        if let actionMark2 = actionMarkField2.text, actionMark2.count <= 1 {
-            action2.mark = actionMark2
-        } else {// 1文字より多い場合のエラー
-            print("アクションマークは1文字の絵文字である必要があります。")
-        }
-        // Realmデータベースにアクションを保存
-        //　realm上にこの書き込む宣言
         let realm = try! Realm()
-        // writeメソッドを使って書き込みトランザクションを開始
         try! realm.write {
-            //　ブジェクト（）をrealmデータベースに保存
             realm.add(action1)
             realm.add(action2)
         }
-        print("text: \(action1.mark), recordDate: \(action2.mark)")
+        
+        print("保存されたアクション: mark1: \(action1.mark), mark2: \(action2.mark)")
     }
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+           saveActions()
+       }
 }
 
-extension ActionEditViewController: UITextViewDelegate {
-    func textviewDidChange(_ textView: UITextView) {
-        let updatedAction = textView.text ?? ""
-        saveActions(with: updatedAction)
-    }
-}
+//キーボードに閉じるボタンがない。マーク２のテキストフィールドはキーボードが出ると、キーボードに隠れてしまう時どうすればいいですか？なお、フィールドの位置は変えたくありません
+//保存されたアクションをコンソール出力されたい。
